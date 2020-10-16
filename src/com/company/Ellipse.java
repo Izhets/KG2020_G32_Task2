6e5bad43ee26888cc5e7193a61b58d4304c8d9f9
Izhets <hnykin.den@yandex.ru>
2020-10-16 23:22:30 +0300

    UPD: Начал эллипсы

---
+++ src/com/company/Ellipse.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Ellipse {
+    private int x1, y1, x2, y2, xc, yc, a, b, ACosMinAngle, ACosMaxAngle, BSinMinAngle, BSinMaxAngle;
+    private double minAngle, maxAngle, deltaAngle,
+            sinMinAngle, sinMaxAngle, cosMinAngle, cosMaxAngle;
+
+    Ellipse(int x1, int y1, int x2, int y2, double startAngle, double endAngle) {
+    }
+}
---
+++ src/com/company/EllipseDrawer.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public interface EllipseDrawer {
+    void drawEllipse(int x1, int y1, int x2, int y2);
+
+    void drawArc(int x1, int y1, int x2, int y2, double startAngle, double endAngle);
+}
